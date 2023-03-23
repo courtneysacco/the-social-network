254f6c6281f3258f32f610001ddcc1eb5ab85041
courtney <xoc0urtney14xo@aim.com>
2023-03-23 17:46:24 -0400

    readme

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://devin1578:Frustr8d@socialmediacluster.smojma3.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
